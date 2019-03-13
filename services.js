780141a3edf188cc6323df7b770f14bdf54044e9
Noybu <48063366+Noybu@users.noreply.github.com>
2019-03-13 19:30:37 +0200

    up

---
+++ services.js
@@ -0,0 +1,16 @@
+
+var img1 =document.getElementById("img1");
+var img2 =document.getElementById("img2");
+
+function overMe()
+{
+	img1.src="img/noentry.png";
+	
+}
+
+function overMe2()
+{
+	img2.src="img/noentry.png";
+	
+}
+
